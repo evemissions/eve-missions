3f3e0c5d4a228e8174d8eb161ac0b1f70134ffa0
Will Wright <dynamite874@tntuniverse.co.uk>
2014-12-27 19:17:02 +0000

    wrong domain again :P

--- auth/devlogin.php
+++ auth/devlogin.php
@@ -8,18 +8,18 @@ if (isset($_SESSION['auth_characterid'])) {
     $authsite='https://login.eveonline.com';
     $authurl='/oauth/authorize';
     $client_id='ea09a57ef06543fea25d389a07d0c4f4';
-    $redirect_uri="http://www.test.eve-missions.com/auth/devauthcallback.php";
+    $redirect_uri="http://test.eve-missions.com/auth/devauthcallback.php";
     $state=uniqid();
 
     $redirecturl=$_SERVER['HTTP_REFERER'];
 
-    if (!preg_match("#^https:/www.test.eve-missions.com/(.*)$#", $redirecturl, $matches)) {
+    if (!preg_match("#^https://test.eve-missions.com/(.*)$#", $redirecturl, $matches)) {
         $redirecturl='/';
     } else {
         $redirecturl=$matches[1];
     }
 
-    $redirect_to="https://www.test.eve-missions.com".$redirecturl;
+    $redirect_to="https://test.eve-missions.com".$redirecturl;
     $_SESSION['auth_state']=$state;
     $_SESSION['auth_redirect']=$redirect_to;
     session_write_close();
