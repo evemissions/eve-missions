45aae16f5ed61182050ed3bc60fdea7cb5cd0bcd
Will Wright <dynamite874@tntuniverse.co.uk>
2014-12-27 19:29:23 +0000

    grrr

--- auth/devlogin.php
+++ auth/devlogin.php
@@ -8,7 +8,7 @@ if (isset($_SESSION['auth_characterid'])) {
     $authsite='https://login.eveonline.com';
     $authurl='/oauth/authorize';
     $client_id='ea09a57ef06543fea25d389a07d0c4f4';
-    $redirect_uri="http://test.eve-missions.com/auth/devauthcallback.php";
+    $redirect_uri="https://test.eve-missions.com/auth/devauthcallback.php";
     $state=uniqid();
 
     $redirecturl=$_SERVER['HTTP_REFERER'];
