04d3949c38229aefbe71d6faaf0df5b00d91c956
greenlittleapple <greenlittleapple@gmail.com>
2015-01-01 17:04:06 -0800

    Quick fix

--- Scripts/acceptMission.php
+++ Scripts/acceptMission.php
@@ -4,7 +4,7 @@ require_once("../db.php");
 $db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 $id_get = htmlspecialchars($_GET['id']);
-Disabled for testing $charName =  $_SESSION['charactername'];
+$charName =  $_SESSION['auth_charactername'];
 
 # Queries for interacting with both databases
 $missionQuery = "SELECT * FROM missions where id='$id_get'";
