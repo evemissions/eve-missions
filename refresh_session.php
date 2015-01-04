18d909435387fcab56cb436b825df19769b5fd74
Will Wright <will@willsworkspace.co.uk>
2015-01-04 08:48:50 +0000

    Updated on git.

--- refresh_session.php
+++ refresh_session.php
@@ -1,11 +1,6 @@
 <?php
   session_start();
-    $_SESSION['auth_characterid'] = 90792652 ;
-    $_SESSION['auth_charactername'] = "vampire Huunuras";
   if (isset($_SESSION['charactername'])) { //if you have more session-vars that are needed for login, also check if they are set and refresh them as well
-    #$_SESSION['charactername'] = $_SESSION['charactername'];
-      # For development only ! :)
-    $_SESSION['auth_characterid'] = 90792652 ;
-    $_SESSION['auth_charactername'] = "vampire Huunuras";
+    $_SESSION['charactername'] = $_SESSION['charactername'];
   }
 ?>
