33e09d87b84b0806b06fd77751c0912814b00175
Will Wright <dynamite874@tntuniverse.co.uk>
2015-01-04 09:15:07 +0000

    Added Admin/index.php to tracker

---
+++ admin/index.php
@@ -0,0 +1,105 @@
+<?php
+require_once("../db.php");
+$db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+session_start();
+$pilotQuery = "SELECT * from user WHERE characterid='" . $_SESSION['auth_characterid'] . "'";
+$pilotRow = mysqli_fetch_array($db_connection->query($pilotQuery));
+var_dump($pilotRow['isAdmin']);
+
+if ($pilotRow['isAdmin'] == false) {
+    header("Location: ../index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="icon" type="image/png" href="img/favicon.ico?v=12.30.10.25">
+    <meta name="google-site-verification" content="Z-sBx9d3kgXBU00XEDE6krv3-hik_uNqF2-amWunO3M" />
+    <link href="/css/bootstrap.min.css?v=1" rel="stylesheet">
+    <link href="/css/flat-ui.min.css?v=1.11" rel="stylesheet">
+    <link href="/css/style.css?v=12.30.10.41" rel="stylesheet" />
+    <meta charset="utf-8">
+    <?php include("../heatmap.php"); ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <title>EVE-Missions</title>
+    <meta name="description" content="Player-created missions with ISK rewards">
+</head>
+
+<body onload="setInterval(function(){$.post('/refresh_session.php');},270);">
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="/" class="navbar-brand">eve-missions</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Current Missions</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="/list.php">All</a>
+                            </li>
+                            <li><a href="#">Top Viewed</a>
+                            </li>
+                            <li><a href="#">Top Rated</a>
+                            </li>
+                            <li><a href="#">Newest</a>
+                            </li>
+                            <li><a href="#">Staff Picks</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="/submit.php">Submit a Mission</a>
+                    </li>
+                    <li><a href="#about" data-toggle="modal">About</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if(!isset($_SESSION['auth_charactername'])) {
+                        echo "<li>
+                            <a href='/auth/devlogin.php'><img src=\"/img/sso.png\">
+                            </a>
+                        </li>";
+                    } else {
+                        echo "<li>
+                            <a style=\"padding-right: 0;\" href='profile.php'>";
+                        echo $_SESSION['auth_charactername'];
+                        echo "&nbsp;&nbsp;<img src=\"https://image.eveonline.com/Character/" . $_SESSION['auth_characterid'] . "_64.jpg\"></a></li>" .
+                            "<li><a href='/auth/logout.php'>(Logout)</a>
+                        </li>";
+                    } ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid jumbotron text-center">
+        <h1>eve-missions</h1>
+        <p style="font-family:lato">Player-created missions with ISK rewards</p>
+    </div>
+    <div class="container" style="text-align:center">
+        <h5>Welcome to EVE-Missions!</h5>
+        <p>EVE-Missions is a player-made website dedicated to offering beginners of the game EVE Online tasks, or missions, that help them learn more about the opportunities available to them. </p>
+        <h5>What is the purpose of EVE-Missions?</h5>
+        <p><b>Our philosophy</b> is that the fun, immersive things you do in EVE Online tend to make your wallet thinner, and boring things usually make it fatter. This deters new players who get stuck running missions in high sec or mining to earn ISK. There is a common misconception that new players are somehow unable to take part in compelling, emergent content - especially by themselves. By offering missions that pay new players to complete fun, "educational" tasks, they can better understand EVE Online in their early days without relying on mining and trying to earn ISK. New players who have never played a sandbox game tend to lack direction, so by providing them the clear-cut objectives and guidance they've come to expect from other modern games, they can have an easier time adjusting to life in New Eden!</p>
+        <h5>Great, how can I sign up?</h5>
+        <p>Just login through EVE Online's secure system on the top right and start accepting missions!</p>
+    </div>
+    <?php include("modal.php"); ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
